ea61dac652e1e356c9ac13e7f1dfd36ee68d860a
bhayron <bhayronklivilan@gmail.com>
2022-02-24 08:59:12 -0300

    Continuacao

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
+
+import { Jogador } from './interfaces/jogador.interface';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+  constructor(private readonly jogadoresService: JogadoresService) {}
   @Post()
   async criarAtualizarJogador(@Body() criaJogadorDto: CriarJogadorDto) {
-    const { email } = criaJogadorDto;
-    return JSON.stringify(`{'email':${email}}`);
+    await this.jogadoresService.criarAtualizarJogador(criaJogadorDto);
+  }
+  async consultarJogadores(): Promise<Jogador[]> {
+    return this.jogadoresService.consultarTodosJogadores();
   }
 }
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -2,16 +2,36 @@ import { Injectable, Logger } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 
+import { v4 as uuidv4 } from 'uuid';
+
 @Injectable()
 export class JogadoresService {
   private jogadores: Jogador[] = [];
   private readonly logger = new Logger(JogadoresService.name);
 
   async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void> {
-    this.logger.log(`criaJogadorDto: ${criaJogadorDto}`);
+    await this.criar(criaJogadorDto);
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return this.jogadores;
   }
 
   private criar(CriaJogadorDto: CriarJogadorDto): void {
     const { nome, email, telefoneCelular } = CriaJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuidv4(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'www.google.com.br/foto123.jpg',
+    };
+    console.log(jogador);
+
+    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
+    this.jogadores.push(jogador);
   }
 }
