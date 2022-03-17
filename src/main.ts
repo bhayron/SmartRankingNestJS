c2e6a11bfaa8d8fd95c81b104a61a9f4e830c1ef
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-03-16 21:55:42 -0300

    Configuracao de timezone para America/Sao_Paulo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  /*
+    Desafio
+    Sobrescrevemos a função toJSON do Date passando um objeto moment. Deste modo 
+    quando o objeto for serializado, ele utilizará o formato de data definido por nós.
+    Todos os objetos Date serão afetados com esta implementação 
+  */
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
+  await app.listen(3000);
 }
 bootstrap();
